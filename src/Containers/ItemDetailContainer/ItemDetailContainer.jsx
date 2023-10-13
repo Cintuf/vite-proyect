e15ec7b16a1e19341d3afd634e03ebf94a7c4799
Cintia <cintiafructuoso@yahoo.com>
2023-10-12 22:19:20 -0300

    main

--- src/Containers/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/Containers/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
-import ItemDetail from '../../components/ItemDetail/ItemDetail'
+import { useParams } from 'react-router-dom'
+import ItemDetail from '../../components/ItemDetail/ItemDetail';
 import { myPromise } from '../../Products/Tragos';
 import './ItemDetailContainer.css'
 
