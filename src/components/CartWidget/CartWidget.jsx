ef49a9b6c8c296fdd1424df96c0e2f97edef3bd0
Cintia Fructuoso Millan <79529651+Cintuf@users.noreply.github.com>
2023-10-24 10:21:27 -0300

    Delete src directory

--- src/components/CartWidget/CartWidget.jsx
+++
@@ -1,9 +0,0 @@
-import './CartWidget.css'
-export const CartWidget = () => {
-  return (
-    <>
-    <i className="fa-solid fa-cart-shopping" id='carrito'></i>
-    <p className="cartItems"></p>
-    </>
-  )
-}
