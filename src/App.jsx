73093e79b98eedf5aa51b3c25f6927ad61d6f950
Cintia Fructuoso Millan <79529651+Cintuf@users.noreply.github.com>
2023-10-23 21:31:31 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
+
 import './App.css'
-import NavBar from './components/NavBar/NavBar'
-import ItemListContainer from './Containers/ItemListContainer/ItemListContainer';
-import { BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
-import ItemDetailContainer from './Containers/ItemDetailContainer/ItemDetailContainer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './containers/ItemListContainer/ItemListContainer';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import CartContainer from './containers/CartContainer/CartContainer';
+import ItemDetailContainer from './containers/ItemDeatilContainer/ItemDetailContainer';
+import { CartContextProvider } from './context/CartContext';
+import Form from './components/Form/Form';
+
 
 function App() {
-
+  let saludo = 'Drink Store'
   return (
-    <BrowserRouter>
-      <NavBar/>
-      <Routes>
-        <Route path="/" element={<ItemListContainer/>} />
-        <Route path="/detail/:id" element={<ItemDetailContainer/>} />
-        <Route path="/category/:id" element={<ItemListContainer/>} />
-      </Routes>
-    </BrowserRouter>
+    <CartContextProvider >
+      <BrowserRouter>
+          <NavBar />
+          <br />
+          <Routes>
+            <Route path='/' element={<ItemListContainer saludo={saludo} />}/>
+            <Route path='/Category/:category' element={<ItemListContainer />}/>
+            <Route path='/detail/:productId' element={<ItemDetailContainer />}/>
+            <Route path='/cart' element={<CartContainer />}/>
+            <Route path='/form' element={<Form/>}/>
+            <Route path='*' element={<Navigate to='/'/>}/>
+          </Routes>
+      </BrowserRouter>
+    </CartContextProvider>   
   )
-
 }
 
 export default App
