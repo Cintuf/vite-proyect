c36350047495ed8a1539b6524e4a2344e3132744
Cintia Fructuoso Millan <79529651+Cintuf@users.noreply.github.com>
2023-11-23 16:24:28 -0300

    Update CartContext.jsx

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -57,7 +57,7 @@ export const CartContextProvider = ({ children }) => {
       .finally(() => {
         vaciarCarrito();
         setShowSuccessMessage(false);
-        history.push("../index.html"); 
+        history.push("https://vite-projects-sigma.vercel.app/"); 
       });
   };
 
