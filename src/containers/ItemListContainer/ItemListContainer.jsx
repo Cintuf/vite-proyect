73a4abe546640127acdab53ea45e8d90649043e1
Cintia Fructuoso Millan <79529651+Cintuf@users.noreply.github.com>
2023-11-23 17:18:29 -0300

    Update ItemListContainer.jsx

--- src/containers/ItemListContainer/ItemListContainer.jsx
+++ src/containers/ItemListContainer/ItemListContainer.jsx
@@ -1,42 +1,43 @@
 import { useState, useEffect } from 'react';
-import './ItemListContainer.css';
-import { useParams } from 'react-router-dom';
-import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore';
-import ItemList from '../../components/ItemList/ItemList';
-import Loading from '../../components/Loading/Loading';
-import Greeting from '../../components/Greeting/Greeting';
+import './ItemListContainer.css'
+import { useParams } from 'react-router-dom'
+import { collection, getDocs, getFirestore, query, where, orderBy } from 'firebase/firestore'
+import ItemList from '../../components/ItemList/ItemList'
+import Loading from '../../components/Loading/Loading'
+import Greeting from '../../components/Greeting/Greeting'
 
 const ItemListContainer = ({ saludo }) => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const { category } = useParams(); 
-  useEffect(() => {
-    const db = getFirestore();
-    const productsCollection = collection(db, 'productos');
+    const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
+    const { category } = useParams() // Cambiado de 'id' a 'category'
 
+    useEffect(() => {
+        const db = getFirestore()
+        const productsCollection = collection(db, 'productos')
 
-    const queryFilter = category
-      ? query(productsCollection, where('Category', '==', category), where('isActive', '==', true))
-      : productsCollection;
+        // Construir la consulta con o sin filtro por categoría
+        const queryFilter = category
+            ? query(productsCollection, where('Category', '==', category), orderBy('Price', 'asc'), where('isActive', '==', true))
+            : productsCollection
 
-    getDocs(queryFilter)
-      .then((data) => setProducts(data.docs.map((product) => ({ id: product.id, ...product.data() }))))
-      .catch((err) => console.log(err))
-      .finally(() => setLoading(false));
-  }, [category]);
+        getDocs(queryFilter)
+            .then(data => setProducts(data.docs.map(product => ({ id: product.id, ...product.data() }))))
+            .catch(err => console.log(err))
+            .finally(() => setLoading(false))
+    }, [category])
 
-  return (
-    <section>
-      {loading ? (
-        <Loading />
-      ) : (
-        <div>
-          <Greeting saludo={saludo} />
-          <ItemList products={products} />
-        </div>
-      )}
-    </section>
-  );
-};
+    return (
+        <section>
+            {loading ?
+                <Loading />
+                :
+                <div>
+                    <Greeting saludo={saludo} />
+                    <ItemList products={products} />
+                </div>
+            }
+        </section>
+    )
+}
 
-export default ItemListContainer;
+export default ItemListContainer
